b5e1022d2c9a617791b4323bd6aef1a25d65472d
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-02-24 12:25:28 -0800

    Added dirs for cwd.

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ function localrc(name, defaults) {
   if(typeof name !== 'string')
     throw new Error('nameless configuration fail')
 
-  var files = require.main.paths.map(function (dir) {
+  var files = [path.join(process.cwd(), 'config')]
+    .concat(
+      require.main.paths.map(function (dir) {
         return dir.split(path.sep).slice(0, -1).concat(['config']).join(path.sep)
-      }).reduce(function (arr, dir) {
-        return arr.concat([
-          path.join(dir, process.arch + '.json'),
-          path.join(dir, process.arch),
-          path.join(dir, process.platform + '.json'),
-          path.join(dir, process.platform),
-          path.join(dir, os.hostname() + '.json'),
-          path.join(dir, os.hostname()),
-          path.join(dir, process.env.USER + '.json'),
-          path.join(dir, process.env.USER),
-          path.join(dir, process.env[name.toUpperCase() + '_ENV'] + '.json'),
-          path.join(dir, process.env[name.toUpperCase() + '_ENV']),
-          path.join(dir, 'defaults.json'),
-          path.join(dir, 'defaults'),
-          path.join(dir, '..', 'config.json')
-        ])
-      }, []).concat([defaults])
+      })
+    )
+    .reduce(function (arr, dir) {
+      return arr.concat([
+        path.join(dir, process.arch + '.json'),
+        path.join(dir, process.arch),
+        path.join(dir, process.platform + '.json'),
+        path.join(dir, process.platform),
+        path.join(dir, os.hostname() + '.json'),
+        path.join(dir, os.hostname()),
+        path.join(dir, process.env.USER + '.json'),
+        path.join(dir, process.env.USER),
+        path.join(dir, process.env[name.toUpperCase() + '_ENV'] + '.json'),
+        path.join(dir, process.env[name.toUpperCase() + '_ENV']),
+        path.join(dir, 'defaults.json'),
+        path.join(dir, 'defaults'),
+        path.join(dir, '..', 'config.json')
+      ])
+    }, [])
+    .concat([defaults])
 
   return cc.apply(null, files).snapshot
 }
